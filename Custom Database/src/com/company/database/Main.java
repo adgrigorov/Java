3d3872fc0f74dad3cb62217889ea086c7565f62e
adgrigorov <55141218+adgrigorov@users.noreply.github.com>
2020-01-24 18:25:05 +0200

    Update Main.java

--- Custom Database/src/com/company/database/Main.java
+++ Custom Database/src/com/company/database/Main.java
@@ -12,37 +12,38 @@ public class Main {
 
         Database database = new Database();
 
-        final Pattern CREATE_QUERY = Pattern.compile("CREATE TABLE (?<tableName>\\S+) \\((?<columns>\\S+)\\)");
-        final Pattern DROP_QUERY = Pattern.compile("DROP TABLE (?<tableName>\\w+)");
+        final Pattern CREATE_QUERY = Pattern.compile("^CREATE TABLE (?<tableName>\\S+) \\((?<columns>\\S+)\\)$");
+        final Pattern DROP_QUERY = Pattern.compile("^DROP TABLE (?<tableName>\\w+)$");
         final String LIST_TABLES = "LIST TABLES";
-        final Pattern TABLE_INFO = Pattern.compile("TABLE INFO (?<tableName>\\w+)");
+        final Pattern TABLE_INFO = Pattern.compile("^TABLE INFO (?<tableName>\\w+)$");
         final Pattern INSERT_QUERY = Pattern.compile("INSERT INTO (?<tableName>\\S+) VALUES (?<values>[\\S, ]+\\))");
-        final Pattern SELECT_QUERY = Pattern.compile("^SELECT (?<rows>\\S+) FROM (?<tableName>\\S+)(( WHERE (?<columnIs>\\S+) (?<condition>[<>!=]{0,2}) (?<thanValue>[\\w\\d]+))?(( ?(?<andOr>AND?|OR)? (?<sColumnIs>\\S+) (?<sCondition>[<>!=]{0,2}) (?<sThanValue>[\\w\\d]+)))?( (?<distinct>DISTINCT))?)?$");
+        final Pattern SELECT_QUERY = Pattern.compile("^SELECT (?<rows>\\S+) FROM (?<tableName>\\S+)(( WHERE (?<rowIs>\\S+) (?<condition>[<>!=]{0,2}) (?<thanValue>[\\w\\d]+))?(( ?(?<andOr>AND?|OR)? (?<sRowIs>\\S+) (?<sCondition>[<>!=]{0,2}) (?<sThanValue>[\\w\\d]+)))?( (?<distinct>DISTINCT))?)?$");
+        final Pattern REMOVE_QUERY = Pattern.compile("^REMOVE FROM (?<tableName>\\S+)( WHERE (?<rowIs>[\\w\\d]+) (?<operator>[<>!=]+) (?<thanValue>\\S+))?( (?<andOr>AND?|OR?) (?<sRowIs>\\S+) (?<sCondition>[<>!=]{0,2}) (?<sThanValue>[\\w\\d]+))?$");
 
         /*
          * Queries are read from console input - several types:
          * --CREATE TABLE - creates a table in the database
          *     example: CREATE TABLE People (ID:Int,Name:String)
-         *              CREATE TABLE People (ID:Int, Name:String) is invalid query -> space between columns
+         *              CREATE TABLE People (ID:Int, Name:String) is not a valid query -> space between columns
          *
          * --DROP TABLE - removes a table from the database
          *     example: DROP TABLE People
          *
-         * --LIST TABLES - shows the name of all tables present in the database         *
+         * --LIST TABLES - shows the names of all tables present in the database
          *
          * --TABLE INFO - gives information about a table and its columns (name and type)
          *     example: TABLE INFO People
          *     expected output: People (ID:int, Name:String)
          *             0 rows total
          *
-         * --INSERT INTO - inserts values into the given table
+         * --INSERT INTO - inserts rows into the given table
          *     example: INSERT INTO People VALUES (1, "Ivan")
          *              INSERT INTO People VALUES (2, "Peter"), (3, "Georgi")
          *              INSERT INTO People VALUES ("Tomas", 4) is not a valid query -> column types do not match
          *              INSERT INTO People VALUES (6) is not a valid query -> column count is different from value count
          *              INSERT INTO People (ID,Name) VALUES (1, Ivan) is not a valid query -> string type values must be
          *                                                            inside quotes: "Ivan" f.e.
-         *              INSERT INTO People (ID, Name) VALUES (4, "Toni") is considered invalid query -> space between columns
+         *              INSERT INTO People (ID, Name) VALUES (4, "Toni") is not a valid query -> space between columns
          *                                                               similar to CREATE TABLE query
          *
          *              expected output from INSERT INTO People VALUES (1, "Ivan"):
@@ -51,6 +52,18 @@ public class Main {
          *                      2 rows inserted
          *              expected output from INSERT INTO People VALUES ("Tomas", 4)
          *                      Incompatible value type to Int column. -> cancels INSERT query
+         *
+         * --REMOVE FROM - removes rows from the given table
+         *      example: REMOVE FROM People     -> removes all rows from TABLE People, but DOES NOT delete the table itself
+         *               REMOVE FROM People WHERE ID = 5 AND Name = "Ivan"
+         *               REMOVE FROM People WHERE ID = 5 OR Name = "Ivan"
+         *               REMOVE * FROM People is not a valid query
+         *               REMOVE FROM People DISTINCT is not a valid query
+         *               REMOVE FROM People WHERE ID = 5 DISTINCT is not a valid query
+         *
+         *      expected output from REMOVE FROM People
+         *          (considering there are 15 rows) 15 rows removed.
+         *
          *
          * --Others to be implemented
          */
@@ -100,9 +113,59 @@ public class Main {
                     String[] values = matcher.group("values")
                             .substring(1, matcher.group("values").length() - 1)
                             .split("\\)\\s?,\\s?\\(");
-                    
+                    //for (String v : values) System.out.println(v);
+                    //System.out.println(values.length);
                     database.insertInto(table, values);
                 }
+            }
+
+            else if (query.contains("SELECT")) {
+                Matcher matcher = SELECT_QUERY.matcher(query);
+
+                if (matcher.find()) {
+                    String[] selectQuery = query.split("\\s+");
+                    int queryLength = selectQuery.length;
+
+                    switch (queryLength) {
+                        case 4: {
+                            String rows = matcher.group("rows");
+                            String table = matcher.group("tableName");
+                            database.selectFrom(rows, table);
+                            //System.out.printf("%s\n%s", rows, table);
+                        }
+                    }
+                }
+            }
+
+            else if (query.contains("REMOVE")) {
+                Matcher matcher = REMOVE_QUERY.matcher(query);
+
+                if (matcher.find()) {
+                    String[] removeQuery = query.split("\\s+");
+                    int queryLength = removeQuery.length;
+
+                    switch (queryLength) {
+                        case 3: {
+                            String table = matcher.group("tableName");
+                            database.removeAllFrom(table);
+                        }
+
+                        case 7: {
+                            String table = matcher.group("tableName");
+                            String whereRowIs = matcher.group("rowIs");
+                            String operator = matcher.group("operator");
+                            String thanValue = matcher.group("thanValue");
+
+                            System.out.println(table);
+                            System.out.println(whereRowIs);
+                            System.out.println(operator);
+                            System.out.println(thanValue);
+
+                            database.removeFromWhere(table, whereRowIs, operator, thanValue);
+                        }
+                    }
+                }
+
             }
 
             else {
